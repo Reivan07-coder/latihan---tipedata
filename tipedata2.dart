6161e3de45db3558a031e1fb74030321835461ed
Reivan07-coder <anthonykhouw07@gmail.com>
2026-09-03 20:37:53 +0700

    versi 3

--- tipedata2.dart
+++ tipedata2.dart
@@ -20,16 +20,24 @@ void main(){
 //   ;
 //   print(produk[1]);
 
-List data = [];
-Map data1 = {"nama":"kopi A", "harga":300000, "stok":100};
-Map data2 = {"nama":"gula", "harga":20000, "stok":10};
-Map data3 = {"nama":"cabe", "harga":10000, "stok":30};
-Map data4 = {"nama":"bawang", "harga":10000, "stok":10};
-Map data5 = {"nama":"bubuk teh", "harga":15000, "stok":200};
-data.add(data1);
-data.add(data2);
-data.add(data3);
-data.add(data4);
-data.add(data5);
-print (data);
+// List data = [];
+// Map data1 = {"nama":"kopi A", "harga":300000, "stok":100};
+// Map data2 = {"nama":"gula", "harga":20000, "stok":10};
+// Map data3 = {"nama":"cabe", "harga":10000, "stok":30};
+// Map data4 = {"nama":"bawang", "harga":10000, "stok":10};
+// Map data5 = {"nama":"bubuk teh", "harga":15000, "stok":200};
+// data.add(data1);
+// data.add(data2);
+// data.add(data3);
+// data.add(data4);
+// data.add(data5);
+// print (data);
+
+// final String nama; //tidak perlu nilai awal
+// nama = "Cindy";
+// nama = "Deka";
+
+// const String nama = "Sindy"; // perlu nilai awal
+// nama = "deka"
+// persamaan const dan final adalah hanya bisa 1x
 }
