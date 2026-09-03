880be275b2146c533ccb6bfbbbc3a8ea58cf6c28
Reivan07-coder <anthonykhouw07@gmail.com>
2026-09-03 19:58:05 +0700

    versi 1

---
+++ tipedata2.dart
@@ -0,0 +1,13 @@
+void main(){
+
+
+  // List data =[];
+  // data.add('merry');
+  // data.add(100);
+  // data.add('Sistem informasi');
+  // data.removeAt(1);
+  // print (data);
+
+Set mhs ={'Dona', 'Martin', 'Gary', 'Windy', 'Gary', 'Windy'};
+print(mhs);
+}
