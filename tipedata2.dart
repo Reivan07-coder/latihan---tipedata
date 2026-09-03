05139799f59e272b219f7cf27ae4be29c7bd7e20
Reivan07-coder <anthonykhouw07@gmail.com>
2026-09-03 20:32:34 +0700

    versi 2

--- tipedata2.dart
+++ tipedata2.dart
@@ -11,5 +11,25 @@ void main(){
 // Set mhs ={'Dona', 'Martin', 'Gary', 'Windy', 'Gary', 'Windy'};
 // print(mhs);
 // map => array index bebas
-Map mhs = {"nama": "Agus", "umur": 30, "prodi": "SI"}
+// Map mhs = {"nama": "Agus", "umur": 30, "prodi": "SI"};
+// List <Map> produk = 
+//   [{"nama":"kopi A", "harga":300000, "stok":100}, {"nama":"gula", "harga":20000, "stok":10},
+//  {"nama":"cabe", "harga":10000, "stok":30},
+//   {"nama":"bawang", "harga":10000, "stok":10},
+//    {"nama":"bubuk teh", "harga":15000, "stok":200}];
+//   ;
+//   print(produk[1]);
+
+List data = [];
+Map data1 = {"nama":"kopi A", "harga":300000, "stok":100};
+Map data2 = {"nama":"gula", "harga":20000, "stok":10};
+Map data3 = {"nama":"cabe", "harga":10000, "stok":30};
+Map data4 = {"nama":"bawang", "harga":10000, "stok":10};
+Map data5 = {"nama":"bubuk teh", "harga":15000, "stok":200};
+data.add(data1);
+data.add(data2);
+data.add(data3);
+data.add(data4);
+data.add(data5);
+print (data);
 }
