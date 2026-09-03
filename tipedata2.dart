9bad868394c65630ea209db596c9990e727c9fef
Reivan07-coder <anthonykhouw07@gmail.com>
2026-09-03 20:05:39 +0700

    versi 1

--- tipedata2.dart
+++ tipedata2.dart
@@ -8,6 +8,8 @@ void main(){
   // data.removeAt(1);
   // print (data);
 
-Set mhs ={'Dona', 'Martin', 'Gary', 'Windy', 'Gary', 'Windy'};
-print(mhs);
+// Set mhs ={'Dona', 'Martin', 'Gary', 'Windy', 'Gary', 'Windy'};
+// print(mhs);
+// map => array index bebas
+Map mhs = {"nama": "Agus", "umur": 30, "prodi": "SI"}
 }
